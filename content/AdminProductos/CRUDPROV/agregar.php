c4a527cbdef35230224fef6ca0f0b2bb897f9f97
KristalPalma <krisprogra@gmail.com>
2024-07-24 01:50:10 -0500

    tengo sueño... cambios en proveedores

--- content/AdminProductos/CRUDPROV/agregar.php
+++ content/AdminProductos/CRUDPROV/agregar.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../conexion.php';
+include '../../conexion.php';
 
 $conexion= $con;
 
@@ -40,11 +40,36 @@ if (mysqli_query($conexion, $peticionInsertar)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Productos</title>
+    <link rel="stylesheet" href="../../../styles/estilos.css">
 </head>
 
 <body>
 
-    <a href="./OpcionesProductos.html">Regresar</a>
+<header :root class="header">
+        <div class="container">
+          <div class="logo">
+            <img src="../../../images/logo orgatito.png" alt="ORGATITO Logo">
+          </div>
+            <nav>
+              <ul>
+                  <li><a class=principal-btn href="../../content/indexproveedor.html">Inicio</a></li>
+                  <li><a class=principal-btn href="../content/AdminProductos/administracion.html">Administración de productos</a></li>
+                  <li><a class=principal-btn href="../../content/AdminProductos/inventario.html">Inventario</a></li>
+                  <li><a class=principal-btn href="../../content/AdminProductos/datospago.html">Datos de pago</a></li>
+              </ul>
+          </nav>
+          <div class="container">
+            <div class="logo">
+              <img src="../../../images/sesionn.png" alt="sesion Logo">
+            </div> 
+        </div>
+    </header>
+
+
+    <br><a class=principal-btn  href="../indexproveedor.html">Regresar</a>
+
+<div class="box-container"><h1>  Agregar productos </h1></div>
+
     <?php foreach ($errores as $error): ?>
         <div style="background-color: black; color: red;"><?php echo $error ?></div>
     <?php endforeach ?>
@@ -53,14 +78,16 @@ if (mysqli_query($conexion, $peticionInsertar)) {
 
     <h1>INGRESO DE PRODUCTOS</h1>
     <h2>En esta sección podrás agregar los detalles de un nuevo producto.</h2>
-    <form action="agregar.php" method="POST">
+
+    <div class="form-container"><form action="agregar.php" method="POST">
 
 
-         <label for="">TIPO</label>
+         <label for="">Tipo</label>
         <select name ="categoria" >
             <option value = "Verdura">Verdura</option>
             <option value = "Fruta">Fruta</option>
         </select>
+        <br></br>
 
         <label for="">NOMBRE DEL PRODUCTO</label>
         <input type="text" name="nombre">
@@ -68,24 +95,32 @@ if (mysqli_query($conexion, $peticionInsertar)) {
 
         <label for="">CANTIDAD</label>
         <input type="int" name="cantidad">
+        <br></br>
 
         <label for="">PRECIO</label>
         <input type="number" name="precio_kilo">
+        <br></br>
 
         <label for="">PROMOCIÓN</label>
         <input type="int" name="promocion">
+        <br></br>
 
         <label for="">TIPO DE ENTREGA</label>
         <select name ="tipo_entrega" >
             <option value = "Local">Local</option>
             <option value = "Domicilio">Domicilio</option>
-        </select>       
+        </select>
+        <br></br>       
 
         <label for="">IMAGEN</label>
         <input type="text" name="imagen">
+        <br></br>
 
         <input type="submit" value="Envíar">
-    </form>
+        <br></br>
+    </form></div>
+
+
 </body>
 
 </html>
